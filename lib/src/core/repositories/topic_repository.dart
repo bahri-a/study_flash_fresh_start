7ff0ed843e93e3380ce62520c88886fa874642e4
Bahri <bahriakcora622@gmail.com>
2026-01-07 16:41:11 +0100

    topicrepo und provider

--- lib/src/core/repositories/topic_repository.dart
+++ lib/src/core/repositories/topic_repository.dart
@@ -9,17 +9,18 @@ class TopicRepository {
 
   TopicRepository(this._coreService, this._authRepository, this.subjectId);
 
-  Future<List> getTopics() async {
+  Future<List<Topic>> getTopics() async {
     final uid = _authRepository.currentUser?.uid;
     if (uid == null) return [];
 
     final rohdaten = await _coreService.getCollection(
-      path: "users/$uid/Subjects/$subjectId/Topic",
+      path: "users/$uid/subjects/$subjectId/Topic",
     );
 
     final rohdatenDocs = rohdaten.docs;
     final topics = rohdatenDocs.map((e) {
       final data = e.data();
+      data['id'] = e.id;
       return Topic.fromJson(data);
     }).toList();
     return topics;
